0a35098a972eb9376e56a8208944798131e2c0a3
woojiahao <woojiahao1234@gmail.com>
2020-04-08 16:16:04 +0800

    add typography plugin + integration w/ gatsby

--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Hi! I'm building a fake Gatsby site as part of this tutorial</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building websites
+    </p>
+  </div>
+)
