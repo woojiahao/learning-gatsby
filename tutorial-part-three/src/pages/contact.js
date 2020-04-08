30cbd1caec414df18742396a121513cd2e97cc34
woojiahao <woojiahao1234@gmail.com>
2020-04-08 16:33:10 +0800

    complete tutorial part 3

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>I'd love to talk! Email me at the address below!</h1>
+    <p>
+      <a href="mailto:example@example.com">example@example.com</a>
+    </p>
+  </Layout>
+)
--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+  <Layout>
     <h1>Hi! I'm building a fake Gatsby site as part of this tutorial</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building websites
     </p>
-  </div>
+  </Layout>
 )
