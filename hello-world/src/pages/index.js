162e274c7c3eaf2dc2758886de0200621041fc89
woojiahao <woojiahao1234@gmail.com>
2020-04-08 14:21:55 +0800

    add: basic hello-world example

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
+)
